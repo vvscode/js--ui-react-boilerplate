a906385a703db6803b938b3625fab936575b2a25
Nikita Gusarov <nickgusarov@gmail.com>
2018-02-20 11:23:47 +0300

    Add state

--- src/components/LoginRoute/index.js
+++ src/components/LoginRoute/index.js
@@ -1,55 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import injectSheet from 'react-jss';
 
-const styles = {
-  greeting: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    width: 400,
-    height: 300,
-    padding: [30, 40],
-    boxSizing: 'border-box',
-    transform: 'translate(-50%, -50%)',
-  },
-  input: {
-    marginBottom: 10,
-  },
-  submitButton: {
-    marginLeft: 'auto',
-  },
-  footer: {
-    display: 'flex',
-    width: '100%',
-    marginTop: 30,
-  },
-};
+import styles from './styles';
 
-const Hello = ({ classes }) => (
-  <Paper className={classes.greeting} elevation={2}>
-    <TextField className={classes.input} label="Email" fullWidth />
-    <TextField
-      className={classes.input}
-      type="password"
-      fullWidth
-      label="Password"
-    />
-    <div className={classes.footer}>
-      <Button color="primary" size="small" className={classes.button}>
-        Forgot password?
-      </Button>
-      <Button variant="raised" color="primary" className={classes.submitButton}>
-        Login
-      </Button>
-    </div>
-  </Paper>
-);
+class LoginRoute extends Component {
+  constructor(...args) {
+    super(...args);
 
-export default injectSheet(styles)(Hello);
+    this.state = {
+      login: '',
+      password: '',
+    };
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { login, password } = this.state;
+
+    return (
+      <Paper className={classes.greeting} elevation={2}>
+        <TextField
+          className={classes.input}
+          label="Email"
+          fullWidth
+          onChange={x => this.setState({ login: x.target.value })}
+          value={login}
+        />
+        <TextField
+          className={classes.input}
+          onChange={x => this.setState({ password: x.target.value })}
+          type="password"
+          fullWidth
+          label="Password"
+          value={password}
+        />
+        <div className={classes.footer}>
+          <Button color="primary" size="small" className={classes.button}>
+            Forgot password?
+          </Button>
+          <Button
+            variant="raised"
+            color="primary"
+            className={classes.submitButton}
+          >
+            Login
+          </Button>
+        </div>
+      </Paper>
+    );
+  }
+}
+
+export default injectSheet(styles)(LoginRoute);
