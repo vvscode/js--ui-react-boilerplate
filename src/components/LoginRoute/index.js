533af662d08ebb74a9f310107d5d543f33eac5db
Nikita Gusarov <nickgusarov@gmail.com>
2018-02-20 09:41:05 +0300

    Add a login form

--- src/components/LoginRoute/index.js
+++ src/components/LoginRoute/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
 import injectSheet from 'react-jss';
 
 const styles = {
   greeting: {
-    color: '#333',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: 400,
+    height: 400,
+    transform: 'translate(-50%, -50%)',
   },
 };
 
 const Hello = ({ classes }) => (
-  <h1 className={classes.greeting}>Login Route</h1>
+  <Paper className={classes.greeting} zDepth={2}>
+    Login Route
+  </Paper>
 );
 
 export default injectSheet(styles)(Hello);
