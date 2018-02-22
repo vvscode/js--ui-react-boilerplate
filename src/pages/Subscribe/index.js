87aa9fda684c36a71abbd5273fee088c99ffcaa8
Nikita Gusarov <nickgusarov@gmail.com>
2018-02-22 11:25:02 +0300

    Add stepper

--- src/pages/Subscribe/index.js
+++ src/pages/Subscribe/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import Stepper, { Step, StepContent, StepLabel } from 'material-ui/Stepper';
 
-const Subscribe = () => <h1>Hello!</h1>;
+const Subscribe = () => (
+  <Stepper activeStep={0} alternativeLabel>
+    <Step>
+      <StepLabel>Choose plan</StepLabel>
+      <StepContent>111</StepContent>
+    </Step>
+    <Step>
+      <StepLabel>Enter payment details</StepLabel>
+      <StepContent>222</StepContent>
+    </Step>
+    <Step>
+      <StepLabel>Enter contact details</StepLabel>
+      <StepContent>333</StepContent>
+    </Step>
+  </Stepper>
+);
 
 export default Subscribe;
